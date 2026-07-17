a307f9edabe2a431a4d931bebf70478e67373b2e
Raviteja T <raviteja.t@nutanix.com>
2026-07-17 07:36:58 +0000

    :q!

--- library/schema.sql
+++ library/schema.sql
@@ -1,9 +1,12 @@
-CREATE TABLE cluster_version (
-        created_at TEXT not null
-      , command TEXT, output TEXT, ip TEXT, cluster_name TEXT);
 CREATE TABLE clusters (
         created_at TEXT not null
-      , uuid TEXT, name TEXT, clusterExternalIPAddress TEXT, fullVersion TEXT);
+      , uuid TEXT, name TEXT, clusterExternalIPAddress TEXT, fullVersion TEXT, pe_ips TEXT);
+CREATE TABLE pgw_status (
+        created_at TEXT not null
+      , status_output TEXT, output TEXT, nos_version TEXT, site_type TEXT);
 CREATE TABLE snapshot_usage (
         created_at TEXT not null
-      , name TEXT, id TEXT, ipv4 TEXT, version TEXT, targetVersion TEXT, externalSubnet TEXT, internalSubnet TEXT, uuid TEXT, output TEXT);
+      , snapshot_reclaimable_bytes TEXT, output TEXT);
+CREATE TABLE vm_count (
+        created_at TEXT not null
+      , total_vms TEXT, output TEXT);
