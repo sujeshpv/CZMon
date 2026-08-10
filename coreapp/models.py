33103f26ebc38c7f9527fbf002183050550b37ec
Raviteja T <raviteja.t@nutanix.com>
2026-08-10 03:48:25 +0000

    2 space ind

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,42 +1,41 @@
 from django.db import models
 
 class PrismGatewayStatus(models.Model):
-    ip_address = models.GenericIPAddressField(unique=True)
-    status_data = models.JSONField(null=True, blank=True)  # Stores nosVersion, clusterFunction, siteType
-    created_at = models.DateTimeField(auto_now=True)
+  ip_address = models.GenericIPAddressField(unique=True)
+  status_data = models.JSONField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'coreapp_prismgatewaystatus'
+  class Meta:
+    db_table = 'coreapp_prismgatewaystatus'
 
 class VMPowerStates(models.Model):
-    ip_address = models.GenericIPAddressField()  # Cluster IP
-    status_data = models.JSONField(null=True, blank=True)  # Stores VM counts and affinity details
-    created_at = models.DateTimeField(auto_now_add=True)
+  ip_address = models.GenericIPAddressField()
+  status_data = models.JSONField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = 'coreapp_vmpowerstates'
+  class Meta:
+    db_table = 'coreapp_vmpowerstates'
 
 class TaskMonitor(models.Model):
-    ip_address = models.GenericIPAddressField()
-    status_data = models.JSONField(null=True, blank=True)  # Stores task list and their statuses
-    created_at = models.DateTimeField(auto_now_add=True)
+  ip_address = models.GenericIPAddressField()
+  status_data = models.JSONField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = 'coreapp_taskmonitor'
+  class Meta:
+    db_table = 'coreapp_taskmonitor'
 
 class AHVHomeUsage(models.Model):
-    cluster_name = models.CharField(max_length=255)
-    status_data = models.JSONField(null=True, blank=True)  # Total, available, and usage % of partitions
-    created_at = models.DateTimeField(auto_now=True)
+  cluster_name = models.CharField(max_length=255)
+  status_data = models.JSONField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'coreapp_ahvhomeusage'
+  class Meta:
+    db_table = 'coreapp_ahvhomeusage'
 
 class UnderutilizedCluster(models.Model):
-    ip_address = models.GenericIPAddressField()
-    status_data = models.JSONField(null=True, blank=True)  # Stores CPU%, Memory%, IOPS, and ALERT flag
-    created_at = models.DateTimeField(auto_now_add=True)
+  ip_address = models.GenericIPAddressField()
+  status_data = models.JSONField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = 'coreapp_underutilizedcluster'
-
+  class Meta:
+    db_table = 'coreapp_underutilizedcluster'
